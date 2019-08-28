0ecd27f1a3b8555af1072788ca8fe1e56fe2adf9
johannes.m.fischer <ga97tay@mytum.de>
2019-08-28 22:23:43 +0200

    set correct emacs formating

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,11 @@
 ;; This file sets the C/C++ Formatting Style in Emacs
 
-((c++-mode . (
-	     (c-file-style . "linux")
-;;	     (c-basic-offset . 4)
-;;	     (tab-width . 4)
-             (c-file-offsets (inline-open . 0))
-	     )))
+((nil . (
+	 (tab-width . 4)
+	 (c-basic-offset . 4)
+	 (c-file-offsets (arglist-intro . '+))
+	 (c-file-offsets (access-label . '-))
+	 (c-file-offsets (member-init-intro . '+))	 
+	 ))
+ (c++-mode . ((c-file-style . "linux")))
+ )
